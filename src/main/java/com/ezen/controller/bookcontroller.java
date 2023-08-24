456fa0df062fa003ee47a400f1d9382d8980edb9
Backsoya <139190275+Backsoya@users.noreply.github.com>
2023-08-24 11:11:37 +0900

    2023/08/24 수정
    Signed-off-by: Backsoya <139190275+Backsoya@users.noreply.github.com>

---
+++ src/main/java/com/ezen/controller/bookcontroller.java
@@ -0,0 +1,47 @@
+package com.ezen.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ezen.book.BookService;
+
+/**
+ * Servlet implementation class bookcontroller
+ */
+@WebServlet("/book")
+public class bookcontroller extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public bookcontroller() {
+	super();
+	// TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+	// get, post 모두 이곳으로 이동된다
+	
+	// 인코딩
+	request.setCharacterEncoding("utf-8");
+	response.setContentType("text/html/;charset=utf");
+	
+	// 서비스 객체 생성 : 수행 메소드 실행
+	
+	// view 변수 : 이동할 jsp 페이지
+	String view=new BookService(request, response).exec();
+		if (view != null) request.getRequestDispatcher(view).forward(request, response);
+	
+    }
+
+}
